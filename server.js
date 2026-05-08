48e70882d16066d66de41c3247047e327f26589a
xyron11 <xxyron604@gmail.com>
2026-05-08 17:03:35 +0700

    fix railway port

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ app.post("/tiktok", (req, res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log("API running")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("API running on port " + PORT)
 })
